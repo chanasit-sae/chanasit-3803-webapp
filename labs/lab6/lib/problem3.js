11a8036148ad3178208cd09a8c569cc98551ce27
peterbackstreet <kyoukai1109@gmail.com>
2026-08-07 12:47:14 +0700

    lab6 problem3 part2_1

--- labs/lab6/lib/problem3.js
+++ labs/lab6/lib/problem3.js
@@ -39,12 +39,15 @@ function promptForOperation() {
   return input;
 }
 
-function promptForNumbers(message) {
+function promptForNumbers(message, mode) {
   var input;
   var valid;
 
   do{
-    input = Number(prompt(message));
+    input = prompt(message);
+    if(mode === 1 && input === "") return input;
+
+    input = Number(input);
     valid = isValidNumber(input);
     if(!valid) alert("Invalid number format")
   }while(!valid);
@@ -56,15 +59,66 @@ function compute(a, b, operation) {
   return operation(a, b);
 }
 
-function main() {
+function performCalculationOnNumbers(numbers, operationCallback){
+  let result = numbers[0];
+  let n = numbers.length;
+  for(let i=1;i<n;i++) {
+    result = operationCallback(result, numbers[i]);
+    if(result === null) return null;
+  }
+
+  return result;
+
+}
+
+function readNumbers(){
+  let numbers = new Array;
+  let input;
+  let valid
+  do{
+    input = promptForNumbers("Enter a number (empty to finish)",1);
+    valid = isValidNumber(input);
+    if(valid) numbers.push(input);
+  }while(valid);
+
+  return numbers;
+}
+
+function runTwoNumberCalculator() {
   let input = promptForOperation();
   let operation = getOperationFunction(input);
-  let a = promptForNumbers("Enter the first number:");
-  let b = promptForNumbers("Enter the second number:");
+  let a = promptForNumbers("Enter the first number:", 0);
+  let b = promptForNumbers("Enter the second number:", 0);
   let result = compute(a, b, operation);
 
   if(result !== null) alert(`The result is ${result}`);
   else alert("Cannot divide by zero");
+}
+
+function runMultipleNumberCalculator() {
+  let op = promptForOperation();
+  let operation = getOperationFunction(op);
+  let numbers = readNumbers();
+  let result = performCalculationOnNumbers(numbers, operation);
+
+  if(result !== null) alert(`The result is ${result}`);
+  else alert("Cannot divide by zero");
+}
+
+function getCalculatorMode() {
+  let input;
+  do{
+    input = Number(prompt("Enter a calculator type"));
+  }while(!input === 0 && !input === 1);
+
+  if(input === 0) return runTwoNumberCalculator();
+  else return runMultipleNumberCalculator();
+
+}
+
+function main() {
+  let calculator = getCalculatorMode();
+  calculator();
 }
 
 main();
